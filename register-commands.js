c81a1ada940dba5f8003e7eecaa92cad19f8a2a7
saehaana <61268356+saehaana@users.noreply.github.com>
2023-03-05 17:47:59 -0500

    moved listeners to separate event folder, implemented file reading for command handling

--- register-commands.js
+++ register-commands.js
@@ -1,15 +1,23 @@
 const {REST, Routes} = require('discord.js');
 const {token, clientID, testGuildID} = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 
-const commands = [
-    {
-        name: 'hey',
-        description: 'replies with greeting',
-    },
-];
+const commands = [];
+
+// Grab all the command files from the commands directory 
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
 
 const rest = new REST({version : '10'}).setToken(token);
 
+//deploy commands
 (async () => {
     try{
         console.log('Registering slash commands..');
