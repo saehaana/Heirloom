fc74483fef5a82e67556fa95858ce6583b9d34d8
saehaana <61268356+saehaana@users.noreply.github.com>
2023-03-06 09:08:44 -0500

    formatted map command reply to have same indents

--- commands/map.js
+++ commands/map.js
@@ -7,7 +7,15 @@ module.exports = {
 		.setName('map')
 		.setDescription('Gets the current map rotation'),
 	async execute(interaction) {
-        const response = await axios.get(`https://api.mozambiquehe.re/maprotation?auth=${apexToken}`);
-        await interaction.reply(`Current: ${response.data.current.map} \nNext: ${response.data.next.map}`);
+        const response = await axios.get(`https://api.mozambiquehe.re/maprotation?version=2&auth=${apexToken}`);
+        await interaction.reply(`Current: ${response.data.ranked.current.map}
+Duration: ${response.data.ranked.current.remainingTimer}  
+
+Next: ${response.data.ranked.next.map}
+
+Current LTM: ${response.data.ltm.current.map}
+Duration: ${response.data.ltm.current.remainingTimer}
+
+Next LTM: ${response.data.ltm.next.map}`);
     },
 };
