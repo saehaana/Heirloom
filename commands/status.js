cd1c8b846e9b746684c49aa2d84f5e8a6037d65a
saehaana <61268356+saehaana@users.noreply.github.com>
2023-03-06 07:25:22 -0500

    added inline fields for server status embed, parsed apex services from get request

--- commands/status.js
+++ commands/status.js
@@ -8,21 +8,19 @@ module.exports = {
 		.setName('status')
 		.setDescription('Checks server status'),
 	async execute(interaction) {
-        let response = await axios.get(`https://api.mozambiquehe.re/servers?auth=${apexToken}`);
-
+        const response = await axios.get(`https://api.mozambiquehe.re/servers?auth=${apexToken}`);
+    
         const embed = new EmbedBuilder()
             .setColor('Green')
-            .setURL('https://apexlegendsstatus.com/')
             .addFields(
-                { name: 'Lobby/Matchmaking Servers', value: 'status'},
-                { name: 'Origin Logins', value: 'status', inline: true },
-                { name: 'EA Accounts', value: 'status', inline: true },
-                { name: 'Apex website', value: 'status', inline: true },
+                { name: 'Lobby/Matchmaking Servers', value: `${response.data.EA_novafusion['US-East'].Status}`},
+                { name: 'Origin Logins', value: `${response.data.Origin_login['US-East'].Status}`, inline: true },
+                { name: 'EA Accounts', value: `${response.data.EA_accounts['US-East'].Status}`, inline: true },
+                { name: 'Apex website', value: `${response.data.selfCoreTest['Status-website'].Status}`, inline: true },
             )
 	        .setTimestamp();
 
         console.log(response.data);
-        //await interaction.reply(`Data from [apexlegendsstatus.com](https://apexlegendsstatus.com/)`);
         await interaction.reply({ embeds: [embed] });
 	},
 };
