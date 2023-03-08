6140df6aab029660f7e1c3070823412aeff40dd3
saehaana <61268356+saehaana@users.noreply.github.com>
2023-03-07 20:57:27 -0500

    map command now displays information in embed

--- commands/craft.js
+++ commands/craft.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const {apexToken} = require('../config.json');
+const { apexToken } = require('../config.json');
 const axios = require('axios');
 
 module.exports = {
@@ -10,35 +10,36 @@ module.exports = {
         let response = await axios.get(`https://api.mozambiquehe.re/crafting?auth=${apexToken}`);
 
 		//Images for each item that holds name and cost data for both daily and weekly crafts
-        //Use same url to add more images to single embed
+        //Uses same url to add more images to single embed
         let dailyEmbed1 = new EmbedBuilder()
         .setTitle('Daily Craft Rotation')
         .setDescription(`
-        ${response.data[0].bundleContent[0].itemType.name}
-        Cost: ${response.data[0].bundleContent[0].cost}
-        ${response.data[0].bundleContent[1].itemType.name}
-        Cost: ${response.data[0].bundleContent[1].cost}`)
-        .setURL(`${response.data[0].bundleContent[0].itemType.asset}`)
-        .setImage(`${response.data[0].bundleContent[0].itemType.asset}`);
+        ${ response.data[0].bundleContent[0].itemType.name }
+        Cost: ${ response.data[0].bundleContent[0].cost }
+        ${ response.data[0].bundleContent[1].itemType.name }
+        Cost: ${ response.data[0].bundleContent[1].cost }`)
+        .setURL(`${ response.data[0].bundleContent[0].itemType.asset }`)
+        .setImage(`${ response.data[0].bundleContent[0].itemType.asset }`);
 
         let dailyEmbed2 = new EmbedBuilder()
-        .setURL(`${response.data[0].bundleContent[0].itemType.asset}`)
-        .setImage(`${response.data[0].bundleContent[1].itemType.asset}`);
+        .setURL(`${ response.data[0].bundleContent[0].itemType.asset }`)
+        .setImage(`${ response.data[0].bundleContent[1].itemType.asset }`);
 
         let weeklyEmbed1 = new EmbedBuilder()
         .setTitle('Weekly Craft Rotation')
         .setDescription(`
-        ${response.data[1].bundleContent[0].itemType.name}
-        Cost: ${response.data[1].bundleContent[0].cost}
-        ${response.data[1].bundleContent[1].itemType.name}
-        Cost: ${response.data[1].bundleContent[1].cost}`)
-        .setURL(`${response.data[1].bundleContent[0].itemType.asset}`)
-        .setImage(`${response.data[1].bundleContent[0].itemType.asset}`);
+        ${ response.data[1].bundleContent[0].itemType.name }
+        Cost: ${ response.data[1].bundleContent[0].cost }
+        ${ response.data[1].bundleContent[1].itemType.name }
+        Cost: ${ response.data[1].bundleContent[1].cost }`)
+        .setURL(`${ response.data[1].bundleContent[0].itemType.asset }`)
+        .setImage(`${ response.data[1].bundleContent[0].itemType.asset }`);
 
         let weeklyEmbed2 = new EmbedBuilder()
-        .setURL(`${response.data[1].bundleContent[0].itemType.asset}`)
-        .setImage(`${response.data[1].bundleContent[1].itemType.asset}`);
+        .setURL(`${ response.data[1].bundleContent[0].itemType.asset }`)
+        .setImage(`${ response.data[1].bundleContent[1].itemType.asset }`);
         
-        await interaction.reply({ embeds: [dailyEmbed1,dailyEmbed2,weeklyEmbed1,weeklyEmbed2]});
+        //Returns discord embeds of all EmbedBuilder objects
+        await interaction.reply({ embeds: [dailyEmbed1,dailyEmbed2,weeklyEmbed1,weeklyEmbed2] });
 	},
 };
--- commands/map.js
+++ commands/map.js
@@ -1,21 +1,42 @@
-const {SlashCommandBuilder} = require('discord.js');
-const {apexToken} = require('../config.json');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { apexToken } = require('../config.json');
 const axios = require('axios');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('map')
-		.setDescription('Gets the current map rotation'),
+		.setDescription('Gets the current map'),
 	async execute(interaction) {
-        const response = await axios.get(`https://api.mozambiquehe.re/maprotation?version=2&auth=${apexToken}`);
-        await interaction.reply(`Current: ${response.data.ranked.current.map}
-Duration: ${response.data.ranked.current.remainingTimer}  
+        let response = await axios.get(`https://api.mozambiquehe.re/maprotation?version=2&auth=${apexToken}`);
 
-Next: ${response.data.ranked.next.map}
+		//EmbedBuilder object that holds maps in rotation for specified game modes
+        let currentMapEmbed = new EmbedBuilder()
+        .setTitle('Ranked Map Rotation')
+        .addFields
+        ({ name: 'Current', value: `${response.data.ranked.current.map}`, inline: true },
+        { name: 'Duration', value: `${response.data.ranked.current.remainingTimer}`, inline: true })
+	    .setImage(`${response.data.ranked.current.asset}`);
 
-Current LTM: ${response.data.ltm.current.map}
-Duration: ${response.data.ltm.current.remainingTimer}
+        let nextMapEmbed = new EmbedBuilder()
+        .addFields
+        ({ name: 'Next', value: `${response.data.ranked.next.map}`, inline: true },
+        { name: 'Start', value: `${response.data.ranked.next.readableDate_start}`, inline: true })
+	    .setImage(`${response.data.ranked.next.asset}`);
 
-Next LTM: ${response.data.ltm.next.map}`);
-    },
+		// let currentDeathmatchEmbed = new EmbedBuilder()
+        // .setTitle('LTM Map Rotation')
+        // .addFields
+        // ({ name: 'Current', value: `${response.data.ltm.current.map}`, inline: true },
+        // { name: 'Duration', value: `${response.data.ltm.current.remainingTimer}`, inline: true })
+	    // .setImage(`${response.data.ltm.current.asset}`);
+
+		// let nextDeathmatchEmbed = new EmbedBuilder()
+        // .addFields
+        // ({ name: 'Next', value: `${response.data.ltm.next.map}`, inline: true },
+        // { name: 'Start', value: `${response.data.ltm.next.readableDate_start}`, inline: true })
+	    // .setImage(`${response.data.ltm.next.asset}`);
+
+        //Returns discord embeds of all EmbedBuilder objects
+        await interaction.reply({ embeds: [currentMapEmbed, nextMapEmbed]});
+	},
 };
