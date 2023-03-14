645249eb2927bcdea2c4e5e462e8f73b7393433a
saehaana <61268356+saehaana@users.noreply.github.com>
2023-03-14 00:29:48 -0400

    updated map time offset conditions and added console logging

--- commands/map.js
+++ commands/map.js
@@ -14,17 +14,22 @@ module.exports = {
 
         // guess() - guesses user's timezone based on user settings
         let timeZone = moment.tz(`${response.data.ranked.next.readableDate_start}`, moment.tz.guess());
+        console.log(timeZone);
 
         // e.g. timeZone = 18:00, offset = -4hrs from 18:00
         let offsetInMinutes = timeZone.utcOffset();
+        console.log(offsetInMinutes);
 
         // Adds or subtracts offset if it is negative or positive to avoid setting wrong times
         // e.g. 18:00 - UTC(-4) = 18:00 + 4:00 = 22:00 | wrong
         //      18:00 - UTC(-4) = 18:00 - 4:00 = 14:00 | correct
         if(offsetInMinutes < 0){
-            formattedTime = timeZone.add(offsetInMinutes, 'minutes').format('MM-DD-YYYY hh:mm:ss A');
-        }else{
-            formattedTime = timeZone.subtract(offsetInMinutes, 'minutes').format('MM-DD-YYYY hh:mm:ss A');
+            formattedTime = timeZone.add(offsetInMinutes, 'minutes').format('MM-DD hh:mm A');
+            console.log(formattedTime);
+        }
+        if(offsetInMinutes >= 0){
+            formattedTime = timeZone.subtract(offsetInMinutes, 'minutes').format('MM-DD hh:mm A');
+            console.log(formattedTime);
         }
         
 		//EmbedBuilder object that holds maps in rotation for specified game modes
