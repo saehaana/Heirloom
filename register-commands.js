6714e05e86f8ae07df5b6057b2aad231fe2d8855
saehaana <61268356+saehaana@users.noreply.github.com>
2023-03-14 18:24:00 -0400

    revert register-commands.js change

--- register-commands.js
+++ register-commands.js
@@ -23,10 +23,15 @@ const rest = new REST({version : '10'}).setToken(token);
         console.log('Registering slash commands..');
         
         await rest.put(
-            Routes.applicationGuildCommands(clientID, (realGuildID && testGuildID)),
+            Routes.applicationGuildCommands(clientID, realGuildID),
             {body : commands}
         );
 
+        await rest.put(
+            Routes.applicationGuildCommands(clientID, testGuildID),
+            {body : commands}
+        );
+        
         console.log('Slash commands registered');
     }catch(error){
         console.log(`error occurred: ${error}`);
