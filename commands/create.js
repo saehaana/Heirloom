32962ef7ff2d16007b6ca873620100c05c6ef602
saehaana <61268356+saehaana@users.noreply.github.com>
2023-03-14 04:25:43 -0400

    added optional flags for create command, embed updates based on optional flags, added dynamic queue number when players join and leave

--- commands/create.js
+++ commands/create.js
@@ -2,14 +2,30 @@ const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, Butt
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('create')
-		.setDescription('Let your friends know if you have room to join their game'),
+		.setName('test')
+		.setDescription('Let your friends know what game you want to play')
+        .addStringOption(option => 
+            option.setName('title')
+            .setDescription('Set a message you want others to see')
+            .setRequired(false))
+        .addIntegerOption(option => 
+            option.setName('team-size')
+            .setDescription('The max number of players allowed')
+            .setRequired(false)),
 	async execute(interaction) {
+        // Holds queue of users who join and leave the lobby
+        const usernames = [];
+
         // Embed that contains current users playing and users wanting to play
         const embed = new EmbedBuilder()
-        .setColor('Blue')
-        .setTitle('Lobby')
-        .setDescription(`Team 1:`);
+        .setColor('Green')
+        .setDescription('**Queue**:')
+
+        // Check if 'title' option was provided and add to embed
+        const titleOption = interaction.options.getString('title');
+        if(titleOption){
+            embed.setTitle(titleOption);
+        }
 
         // Row of buttons that lets users decide if they want to play
         const buttons = new ActionRowBuilder()
@@ -29,25 +45,26 @@ module.exports = {
 
         // Create a message component collector to listen for button clicks
         const filter = i => i.customId === 'join' || i.customId === 'leave';
-        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 300000 });
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 3600000 });
 
-        const usernames = [];
-        
         collector.on('collect', async i => {
+            // 'team-size' optional flag to be used as max size of team
+            const teamSizeOption = interaction.options.getString('team-size');
+
             // Update the embed based on which button was clicked
-            if (i.customId === 'join') {
+            if(i.customId === 'join'){
                 // Ensures only unique names are added to the embed
                 if(!usernames.includes(i.user.username)){
                     usernames.push(i.user.username);
                 }
-                embed.setDescription(`Team 1: \n ${usernames.join('\n')}`);
-            } else if (i.customId === 'leave') {
+                embed.setDescription(`**Queue (${usernames.length} / ${teamSizeOption})**: \n ${usernames.join('\n')}`);
+            }else if(i.customId === 'leave'){
                 // Remove the user's username from the list of joined users
 				const index = usernames.indexOf(i.user.username);
-				if (index !== -1) {
+				if(index !== -1){
 					usernames.splice(index, 1);
 				}
-                embed.setDescription(`Team 1:\n${usernames.join('\n')}`);
+                embed.setDescription(`**Queue (${usernames.length} / ${teamSizeOption})**: \n ${usernames.join('\n')}`);
             }
     
             // Edit the original message with the updated embed
@@ -55,7 +72,8 @@ module.exports = {
         });
     
         collector.on('end', async collected => {
-            embed.setTitle('Lobby (Closed)');
+            embed.setTitle(`${titleOption} (Closed)`);
+            embed.setColor('Red');
             // Remove the buttons from the original message when the collector ends
             await interaction.editReply({ embeds: [embed], components: [] });
         });
