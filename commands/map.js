b936a9d94d1876e6083a29a3c53c9d3be0579acc
saehaana <61268356+saehaana@users.noreply.github.com>
2023-03-26 21:46:37 -0400

    added unranked map rotations to map command

--- commands/map.js
+++ commands/map.js
@@ -6,48 +6,121 @@ const moment = require("moment-timezone");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('map')
-		.setDescription('Gets the current map'),
+		.setDescription('Gets the current and next maps')
+		.addStringOption(option =>
+			option.setName('ranked')
+            .setDescription('Selects the ranked game mode')
+            .addChoices(
+                { name: 'Battle Royale', value: 'ranked_br' },
+                { name: 'Arenas', value: 'ranked_arena' },
+            ))
+        .addStringOption(option =>
+            option.setName('unranked')
+            .setDescription('Selects the unranked game mode')
+            .addChoices(
+                { name: 'Battle Royale', value: 'unranked_br' },
+                { name: 'Arenas', value: 'unranked_arena' },
+            ))
+        .addStringOption(option =>
+            option.setName('ltm')
+            .setDescription('Selects the ltm game mode')),
 	async execute(interaction) {
         let response = await axios.get(`https://api.mozambiquehe.re/maprotation?version=2&auth=${apexToken}`);
 
-        // Get the user's timezone and format it for readability
+        const rankedOption = interaction.options.getString('ranked');
+        const unrankedOption = interaction.options.getString('unranked');
+        const ltmOption = interaction.options.getString('ltm');
 
-        // guess() - guesses user's timezone based on user settings
-        let timeZone = moment.tz(`${response.data.ranked.next.readableDate_start}`, moment.tz.guess());
-        console.log(timeZone);
+        let allEmbeds = [];
 
-        // e.g. timeZone = 18:00, offset = -4hrs from 18:00
-        let offsetInMinutes = timeZone.utcOffset();
-        console.log(offsetInMinutes);
+        // Check if the ranked option was selected
+        if(rankedOption){
 
-        // Adds or subtracts offset if it is negative or positive to avoid setting wrong times
-        // e.g. 18:00 - UTC(-4) = 18:00 + 4:00 = 22:00 | wrong
-        //      18:00 - UTC(-4) = 18:00 - 4:00 = 14:00 | correct
-        if(offsetInMinutes < 0){
-            formattedTime = timeZone.add(offsetInMinutes, 'minutes').format('MM/DD hh:mm A');
-            console.log(formattedTime);
-        }
-        if(offsetInMinutes >= 0){
-            formattedTime = timeZone.subtract(offsetInMinutes, 'minutes').format('MM/DD hh:mm A');
-            console.log(formattedTime);
-        }
-        
-		//EmbedBuilder object that holds maps in rotation for specified game modes
-        let currentMapEmbed = new EmbedBuilder()
-        .setTitle('Ranked Map Rotation')
-        .setColor('Blue')
-        .addFields
-        ({ name: 'Current', value: `${response.data.ranked.current.map}`, inline: true },
-        { name: 'Duration', value: `${response.data.ranked.current.remainingTimer}`, inline: true })
-	    .setImage(`${response.data.ranked.current.asset}`);
+            //                                                      //
+            // Get the user's timezone and format it for readability//
+            //                                                      //
 
-        let nextMapEmbed = new EmbedBuilder()
-        .setColor('Blue')
-        .addFields
-        ({ name: 'Next', value: `${response.data.ranked.next.map}`, inline: true },
-        { name: 'Start', value: `${formattedTime}`, inline: true })
-	    .setImage(`${response.data.ranked.next.asset}`);
+            // Time formatting for ranked battle royale
 
-        await interaction.reply({ embeds: [currentMapEmbed, nextMapEmbed]});
+            // guess() - guesses user's timezone based on user system settings
+            let rankedTime = moment.tz(`${response.data.ranked.next.readableDate_start}`, moment.tz.guess());
+
+            // e.g. timeZone = 18:00, offset = -4hrs from 18:00
+            let rankedOffset = rankedTime.utcOffset();
+
+            // Adds or subtracts offset if it is negative or positive to avoid setting wrong times
+            // e.g. 18:00 - UTC(-4) = 18:00 + 4:00 = 22:00 | wrong
+            //      18:00 - UTC(-4) = 18:00 - 4:00 = 14:00 | correct
+            if(rankedOffset < 0){
+                rankedFormatted = rankedTime.add(rankedOffset, 'minutes').format('MM/DD hh:mm A');
+            }
+            if(rankedOffset >= 0){
+                rankedFormatted = rankedTime.subtract(rankedOffset, 'minutes').format('MM/DD hh:mm A');
+            }
+
+            // Creates and displays embeds of current and next maps for the specified game mode
+
+            const currentRankedBrEmbed = new EmbedBuilder()
+            .setTitle('Ranked Map Rotation')
+            .setColor('Blue')
+            .addFields
+            ({ name: 'Current', value: `${response.data.ranked.current.map}`, inline: true },
+            { name: 'Duration', value: `${response.data.ranked.current.remainingTimer}`, inline: true })
+            .setImage(`${response.data.ranked.current.asset}`);
+
+            allEmbeds.push(currentRankedBrEmbed);
+
+            const nextRankedBrEmbed = new EmbedBuilder()
+            .setColor('Blue')
+            .addFields
+            ({ name: 'Next', value: `${response.data.ranked.next.map}`, inline: true },
+            { name: 'Start', value: `${rankedFormatted}`, inline: true })
+            .setImage(`${response.data.ranked.next.asset}`);
+
+            allEmbeds.push(nextRankedBrEmbed);
+        } 
+
+        // Check if the unranked option was selected
+        if(unrankedOption){
+            // Time formatting for unranked battle royale
+
+            let unrankedTime = moment.tz(`${response.data.battle_royale.next.readableDate_start}`, moment.tz.guess());
+            console.log(unrankedTime);
+
+            let unrankedOffset = unrankedTime.utcOffset();
+            console.log(unrankedOffset);
+
+            if(unrankedOffset < 0){
+                unrankedFormatted = unrankedTime.add(unrankedOffset, 'minutes').format('hh:mm A');
+                console.log(unrankedFormatted);
+            }
+            if(unrankedOffset >= 0){
+                unrankedFormatted = unrankedTime.subtract(unrankedOffset, 'minutes').format('hh:mm A');
+                console.log(unrankedFormatted);
+            }
+
+            // Creates and displays embeds of current and next maps for the specified game mode
+
+            const currentUnrankedBrEmbed = new EmbedBuilder()
+            .setTitle('Unranked Map Rotation')
+            .setColor('Blue')
+            .addFields
+            ({ name: 'Current', value: `${response.data.battle_royale.current.map}`, inline: true },
+            { name: 'Duration', value: `${response.data.battle_royale.current.remainingMins} minute(s)`, inline: true })
+            .setImage(`${response.data.battle_royale.current.asset}`);
+
+            allEmbeds.push(currentUnrankedBrEmbed);
+            
+            const nextUnrankedBrEmbed = new EmbedBuilder()
+            .setColor('Blue')
+            .addFields
+            ({ name: 'Next', value: `${response.data.battle_royale.next.map}`, inline: true },
+            { name: 'Start', value: `${unrankedFormatted}`, inline: true })
+            .setImage(`${response.data.battle_royale.next.asset}`);
+
+            allEmbeds.push(nextUnrankedBrEmbed);
+        } 
+
+        await interaction.reply({ embeds: allEmbeds });
 	},
 };
