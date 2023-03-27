eda00047794222bd808a0c6f4683ea6cfd171171
saehaana <61268356+saehaana@users.noreply.github.com>
2023-03-27 01:57:57 -0400

    updated craft command to include daily and weekly choices, edited embeds to reflect choice selected, moved item name and cost from embed description to fields

--- commands/craft.js
+++ commands/craft.js
@@ -5,43 +5,61 @@ const axios = require('axios');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('craft')
-		.setDescription('Gets the current replicator items'),
+		.setDescription('Gets the current replicator items')
+        .addStringOption(option =>
+			option.setName('type')
+            .setDescription('View craft rotations ')
+            .addChoices(
+                { name: 'Daily', value: 'daily' },
+                { name: 'Weekly', value: 'weekly' },
+            )),
 	async execute(interaction) {
         let response = await axios.get(`https://api.mozambiquehe.re/crafting?auth=${apexToken}`);
 
-		//Images for each item that holds name and cost data for both daily and weekly crafts
-        //Uses same url to add more images to single embed
-        let dailyEmbed1 = new EmbedBuilder()
-        .setTitle('Daily Craft Rotation')
-        .setColor('Blue')
-        .setDescription(`
-        ${ response.data[0].bundleContent[0].itemType.name }
-        Cost: ${ response.data[0].bundleContent[0].cost }
-        ${ response.data[0].bundleContent[1].itemType.name }
-        Cost: ${ response.data[0].bundleContent[1].cost }`)
-        .setURL(`${ response.data[0].bundleContent[0].itemType.asset }`)
-        .setImage(`${ response.data[0].bundleContent[0].itemType.asset }`);
+        const choice = interaction.options.getString('type');
+        let allEmbeds = [];
 
-        let dailyEmbed2 = new EmbedBuilder()
-        .setURL(`${ response.data[0].bundleContent[0].itemType.asset }`)
-        .setImage(`${ response.data[0].bundleContent[1].itemType.asset }`);
+        if(choice == 'daily'){
+            //Images for each item that holds name and cost data for both daily and weekly crafts
+            //Uses same url to add more images to single embed
+            let dailyEmbed1 = new EmbedBuilder()
+            .setTitle('Daily Craft Rotation')
+            .setColor('Blue')
+            .setURL(`${ response.data[0].bundleContent[0].itemType.asset }`)
+            .setImage(`${ response.data[0].bundleContent[0].itemType.asset }`)
+            .addFields
+            ({ name: `${response.data[0].bundleContent[0].itemType.name}`, value: `Cost: ${response.data[0].bundleContent[0].cost}`, inline: true },
+            { name: `${response.data[0].bundleContent[1].itemType.name}`, value: `Cost: ${ response.data[0].bundleContent[1].cost }`, inline: true })
+            
+            allEmbeds.push(dailyEmbed1);
 
-        let weeklyEmbed1 = new EmbedBuilder()
-        .setTitle('Weekly Craft Rotation')
-        .setColor('Blue')
-        .setDescription(`
-        ${ response.data[1].bundleContent[0].itemType.name }
-        Cost: ${ response.data[1].bundleContent[0].cost }
-        ${ response.data[1].bundleContent[1].itemType.name }
-        Cost: ${ response.data[1].bundleContent[1].cost }`)
-        .setURL(`${ response.data[1].bundleContent[0].itemType.asset }`)
-        .setImage(`${ response.data[1].bundleContent[0].itemType.asset }`);
+            let dailyEmbed2 = new EmbedBuilder()
+            .setURL(`${ response.data[0].bundleContent[0].itemType.asset }`)
+            .setImage(`${ response.data[0].bundleContent[1].itemType.asset }`);
 
-        let weeklyEmbed2 = new EmbedBuilder()
-        .setURL(`${ response.data[1].bundleContent[0].itemType.asset }`)
-        .setImage(`${ response.data[1].bundleContent[1].itemType.asset }`);
-        
+            allEmbeds.push(dailyEmbed2);
+        }
+
+        if(choice == 'weekly'){
+            let weeklyEmbed1 = new EmbedBuilder()
+            .setTitle('Weekly Craft Rotation')
+            .setColor('Blue')
+            .setURL(`${ response.data[1].bundleContent[0].itemType.asset }`)
+            .setImage(`${ response.data[1].bundleContent[0].itemType.asset }`)
+            .addFields
+            ({ name: `${response.data[1].bundleContent[0].itemType.name}`, value: `Cost: ${response.data[1].bundleContent[0].cost}`, inline: true },
+            { name: `${ response.data[1].bundleContent[1].itemType.name}`, value: `Cost: ${ response.data[1].bundleContent[1].cost}`, inline: true })
+
+            allEmbeds.push(weeklyEmbed1);
+    
+            let weeklyEmbed2 = new EmbedBuilder()
+            .setURL(`${ response.data[1].bundleContent[0].itemType.asset }`)
+            .setImage(`${ response.data[1].bundleContent[1].itemType.asset }`);
+
+            allEmbeds.push(weeklyEmbed2);
+        }
+
         //Returns discord embeds of all EmbedBuilder objects
-        await interaction.reply({ embeds: [dailyEmbed1,dailyEmbed2,weeklyEmbed1,weeklyEmbed2] });
+        await interaction.reply({ embeds: allEmbeds });
 	},
 };
