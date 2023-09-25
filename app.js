5431b2a427024f2b8dfd5039a7e1d6948a1fc8a2
saehaana <61268356+saehaana@users.noreply.github.com>
2023-09-25 14:16:39 -0400

    Added test-commands directory as event files

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ const commandsPath = path.join(__dirname, 'commands');
 //reads path to directory and returns array of all javascript files  
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+const testCommandsPath = path.join(__dirname, 'test-commands');  
+const testCommandFiles = fs.readdirSync(testCommandsPath).filter(file => file.endsWith('.js'));
+
 //get event files
 //constructs path to events directory
 const eventsPath = path.join(__dirname, 'events');
@@ -34,6 +37,16 @@ for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+
+for (const file of testCommandFiles) {
+	const filePath = path.join(testCommandsPath, file);
+	const command = require(filePath);
 	if ('data' in command && 'execute' in command) {
 		client.commands.set(command.data.name, command);
 	} else {
